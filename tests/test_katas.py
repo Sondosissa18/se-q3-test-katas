70ee20f00008b5da83150fcdef6c422ee12c0e6b
SONDOS <sondosissa18@gmail.com>
2020-09-07 10:01:51 -0500

    passed all tests

--- tests/test_katas.py
+++ tests/test_katas.py
@@ -1,22 +1,35 @@
+
+__author__ = "sondos with help from gabby and shanquel"
+
 import unittest
 import katas
 
 
 class TestKatas(unittest.TestCase):
     def test_add(self):
-        self.fail("TODO: Write add unit test")
+        self.assertEqual(katas.add(2, 1), 3)
+        self.assertEqual(katas.add(-8, 4), -4)
+        self.assertEqual(katas.add(120, 16), 136)
 
     def test_multiply(self):
-        self.fail("TODO: Write multiply unit test")
+        self.assertEqual(katas.multiply(5, 7), 35)
+        self.assertEqual(katas.multiply(-8, 4), -32)
+        self.assertEqual(katas.multiply(4, 4), 16)
 
     def test_power(self):
-        self.fail("TODO: Write power unit test")
+        self.assertEqual(katas.power(2, 3), 8)
+        self.assertEqual(katas.power(3, 3), 27)
+        self.assertEqual(katas.power(5, 2), 25)
 
     def test_factorial(self):
-        self.fail("TODO: Write factorial unit test")
+        self.assertEqual(katas.factorial(5), 120)
+        self.assertEqual(katas.factorial(2), 2)
+        self.assertEqual(katas.factorial(3), 6)
 
     def test_fibonacci(self):
-        self.fail("TODO: Write fibonacci unit test")
+        self.assertEqual(katas.fibonacci(15), 377)
+        self.assertEqual(katas.fibonacci(8), 13)
+        self.assertEqual(katas.fibonacci(10), 34)
 
 
 if __name__ == '__main__':
